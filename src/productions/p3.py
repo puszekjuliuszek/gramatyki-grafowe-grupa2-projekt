c070cd94f44d6017e2e8d4b9d4a177e7f1b2914e
Jakub Płowiec <frytkaspl@gmail.com>
2025-12-18 05:01:56 +0100

    p3 fixed with basic tests

--- src/productions/p3.py
+++ src/productions/p3.py
@@ -25,14 +25,19 @@ class P3(Production):
         edge = left.hyperedges[0]
         n1, n2 = edge.nodes
 
+        g.add_node(n1)
+        g.add_node(n2)
+        edge.r=0
+        g.add_edge(edge)
+
         new_x = (n1.x + n2.x) / 2
         new_y = (n1.y + n2.y) / 2
         
         new_node = Node(new_x, new_y, "n3")
 
         g.add_node(new_node)
-        e1 = HyperEdge((n1, new_node), "E", r=0)
-        e2 = HyperEdge((new_node, n2), "E", r=0)
+        e1 = HyperEdge((n1, new_node), "E", r=0, b=edge.b)
+        e2 = HyperEdge((new_node, n2), "E", r=0, b=edge.b)
 
         g.add_edge(e1, check_nodes=False)
         g.add_edge(e2, check_nodes=False)
@@ -49,7 +54,7 @@ class P3(Production):
             return False
             
         # Check attributes r=1 and b=0
-        if getattr(edge, "r", 0) != 1:
+        if getattr(edge, "r", 0) != 1 or getattr(edge, "b", 1) != 0:
             return False
             
         return True
--- test/test_p3.py
+++ test/test_p3.py
@@ -26,7 +26,7 @@ class TestP3Case1:
         self.g.add_node(n1)
         self.g.add_node(n2)
 
-        self.g.add_edge(HyperEdge((n1, n2), "E", r=1))
+        self.g.add_edge(HyperEdge((n1, n2), "E", r=1, b=0))
 
         self.p3 = P3()
 
@@ -39,4 +39,65 @@ class TestP3Case1:
         assert cnt.hyper == 1
 
         edges = self.g.hyperedges
+        assert len(edges) == 1
+        assert edges[0].hypertag == "E"
         assert edges[0].r == 1
+        assert edges[0].b == 0
+    
+    def test_stage1(self):
+        applied = self.g.apply(self.p3)
+
+        draw(self.g, str(DRAW_DIR / "test3-case1-stage1.png"))
+
+        assert applied == 1, "Production P3 should be applied exactly once"
+
+        cnt = self.g.count_nodes()
+        assert cnt.normal == 3
+        assert cnt.hyper == 3
+
+        edges = self.g.hyperedges
+        for e in edges:
+            assert e.hypertag == "E", f"All edges must be E"
+            assert e.r == 0, f"All E edges must have r=0"
+            assert e.b == 0, f"All edges must have b=0"
+
+class TestP3Case2:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.g = Graph()
+
+        n1 = Node(0, 0, "n1")
+        n2 = Node(0, 4, "n2")
+
+        self.g.add_node(n1)
+        self.g.add_node(n2)
+
+        self.g.add_edge(HyperEdge((n1, n2), "E", r=0, b=0))
+
+        self.p3 = P3()
+
+    def test_stage0(self):
+        """Test input graph."""
+        draw(self.g, str(DRAW_DIR / "test3-case2-stage0.png"))
+
+        cnt = self.g.count_nodes()
+        assert cnt.normal == 2
+        assert cnt.hyper == 1
+
+        edges = self.g.hyperedges
+        assert len(edges) == 1
+        assert edges[0].hypertag == "E"
+        assert edges[0].r == 0
+        assert edges[0].b == 0
+    
+    def test_stage1(self):
+        applied = self.g.apply(self.p3)
+
+        draw(self.g, str(DRAW_DIR / "test3-case2-stage1.png"))
+
+        assert applied == 0, "Production P3 should be applied exactly once"
+
+        cnt = self.g.count_nodes()
+        assert cnt.normal == 2
+        assert cnt.hyper == 1
+
