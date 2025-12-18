ced301f51bc26e944edbd2acf6e449a4b222fe14
Rys-Nowak <ryszard.nowak012@gmail.com>
2025-12-18 17:11:17 +0100

    p4

---
+++ src/productions/p4.py
@@ -0,0 +1,72 @@
+"""
+Production P4
+
+Breaks boundary edges (E with b=1) marked for refinement (r=1)
+"""
+
+from edge import HyperEdge
+from graph import Graph
+from node import Node
+from productions.production import Production
+
+
+@Production.register
+class P4(Production):
+    """
+    Production P4 - Break boundary edges
+
+    Breaks boundary edges (E with b=1) marked for refinement (r=1)
+    """
+
+    def get_left_side(self) -> Graph:
+        """
+        Creates the left side of the production.
+
+        Returns:
+            Left side graph
+        """
+        g = Graph()
+
+        n1 = Node(0, 0, "n1")
+        n2 = Node(0, 1, "n2")
+
+        g.add_node(n1)
+        g.add_node(n2)
+
+        g.add_edge(HyperEdge((n1, n2), "E", r=1, b=1))
+
+        return g
+
+    def get_right_side(self, left: Graph) -> Graph:
+        """
+        Creates the right side of the production.
+
+        Args:
+            left: Matched subgraph (with current coordinates)
+
+        Returns:
+            Right side graph with new vertex breaking edge in half
+        """
+        g = Graph()
+
+        nodes = left.nodes.values()
+
+        new_x = (nodes[0].x + nodes[1].x) / 2
+        new_y = (nodes[0].y + nodes[1].y) / 2
+        new_node = Node(new_x, new_y, "n3")
+        nodes.append(new_node)
+
+        for node in nodes:
+            g.add_node(node)
+
+        g.add_edge(HyperEdge((nodes[0], nodes[2]), "E", r=0, b=1), check_nodes=False)
+        g.add_edge(HyperEdge((nodes[0], nodes[1]), "E", r=0, b=1), check_nodes=False)
+
+        return g
+
+    def filter_match(self, matched_graph: Graph) -> bool:
+        """r == 1 AND b == 1"""
+        for edge in matched_graph.hyperedges:
+            if edge.r != 1 or edge.b != 1:
+                return False
+        return True
