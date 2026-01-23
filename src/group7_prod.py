8b48de88c9cbd1d3094b96c66174fc3c099df0e9
Stratos <filipek.gracjan@o2.pl>
2026-01-23 11:42:40 +0100

    Code state after 23.01 class

--- src/group7_prod.py
+++ src/group7_prod.py
@@ -3,6 +3,9 @@ from node import Node
 from edge import HyperEdge
 from graph import Graph
 from visualization import draw
+from productions.p0 import P0
+from productions.p6 import P6
+from productions.p9 import P9
 
 DRAW_DIR = Path(__file__).parent.parent / "draw" / "group7_prod"
 DRAW_DIR.mkdir(exist_ok=True)
@@ -10,7 +13,7 @@ DRAW_DIR.mkdir(exist_ok=True)
 g = Graph()
 
 # -----------------
-# Outer octagon (12 nodes)
+# Outer contour (7 nodes)
 # -----------------
 n1  = Node(12, 3, "n1")
 n2  = Node(9, 0, "n2")
@@ -57,7 +60,7 @@ g.add_edge(HyperEdge((c, d), "E", r=0))
 g.add_edge(HyperEdge((d, a), "E", r=0))
 
 # -----------------
-# Diagonal connections (exactly as in image)
+# Diagonal connections
 # -----------------
 g.add_edge(HyperEdge((a, n6), "E", r=0))
 g.add_edge(HyperEdge((b, n7), "E", r=0))
@@ -65,17 +68,66 @@ g.add_edge(HyperEdge((c, n2),  "E", r=0))
 g.add_edge(HyperEdge((d, n3),  "E", r=0))
 
 # -----------------
-# Central face hyperedge
+# Central face hyperedges
 # -----------------
+
+# Upper quadrilateral
+g.add_edge(
+    HyperEdge(
+        (a, n6, n7, b),
+        "Q",
+        r=0
+    )
+)
+
+# Central quadrilateral
 g.add_edge(
     HyperEdge(
         (a, b, c, d),
         "S",
-        r=1
+        r=0
+    )
+)
+
+# Lower quadrilateral
+g.add_edge(
+    HyperEdge(
+        (n2, n3, d, c),
+        "Q",
+        r=0
+    )
+)
+
+# Pentagon
+g.add_edge(
+    HyperEdge(
+        (n2, c, b, n7, n1),
+        "P",
+        r=0
+    )
+)
+
+# Hexagon
+g.add_edge(
+    HyperEdge(
+        (n3, n4, n5, n6, a, d),
+        "S",
+        r=0
     )
 )
 
 # -----------------
 # Draw
 # -----------------
-draw(g, str(DRAW_DIR / "group7_prod.png"))
+draw(g, str(DRAW_DIR / "group7_0.png"))
+
+p6 = P6()
+p6.apply(g)
+draw(g, str(DRAW_DIR / "group7_1.png"))
+
+# P0 is applied to each subgraph, but should be applied only to one!
+# TODO: do soemthing about it
+p0 = P0()
+g.apply(p0)
+draw(g, str(DRAW_DIR / "group7_2.png"))
+
